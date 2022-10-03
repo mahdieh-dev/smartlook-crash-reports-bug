fbab8d2f629fe095f0f5ad59e86dc741f0ba5993
Mahdieh Shavandi <mdiehsh@gmail.com>
2022-10-03 17:07:26 +0330

    initial commit

---
+++ App.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+
+import Config from 'react-native-config';
+
+const App = () => {
+  const initSmartlook = useCallback(() => {
+    if (isProd) {
+      Smartlook.setupAndStartRecording(Config.SMART_LOOK_KEY);
+      Smartlook.registerIntegrationListener(
+        async function (dashboardSessionUrl) {
+          await Promise.all([
+            crashlytics().setAttribute(
+              'dashboardSessionUrl',
+              dashboardSessionUrl,
+            ),
+          ]);
+        },
+        async function (dashboardVisitorUrl) {
+          await Promise.all([
+            crashlytics().setAttribute(
+              'dashboardVisitorUrl',
+              dashboardVisitorUrl,
+            ),
+          ]);
+        },
+      );
+    }
+  }, [isProd]);
+
+  useEffect(() => {
+    initSmartlook();
+  }, []);
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {flex: 1},
+});
+
+export default App;
